3694f7f4bc6fdd63de8c323304d6968452bb3bae
calvin <puram.calvin@gmail.com>
2020-04-28 12:26:58 +0100

    enchance service worker

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Bangers", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
